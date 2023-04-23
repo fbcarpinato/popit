67dfb04ae3594f8eaef9b1bd89b39bcee8bb2459
Fabio Carpinato <fabiocarpinato@outlook.it>
2023-04-23 10:49:01 +0200

    feat(api): adds endpoint to fetch logged in user profile

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -19,7 +19,7 @@ export class AuthService {
   }
 
   async login(user: User) {
-    const payload = { username: user.username, sub: user.id };
+    const payload = { email: user.email, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
---
+++ apps/api/src/app/users/users.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Request() request) {
+    return this.usersService.findOne(request.user.email);
+  }
+}
